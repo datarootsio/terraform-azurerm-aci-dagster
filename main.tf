f693b39ab53171132e32f563a3f086b6a92fcca8
Sandervg4 <sander@dataroots.io>
2021-04-26 17:02:38 +0200

    changed the required version to also support newer releases

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.56.0"
+      version = ">=2.56.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
+//# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
